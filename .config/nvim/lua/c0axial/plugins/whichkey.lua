68a43c8f6aae75a4410c5ee0820da7d269ed255c
Alexandre Costa <alex@alexcosta.dev>
2025-02-16 21:47:38 +0000

    chore(nvim): add which-key

---
+++ .config/nvim/lua/c0axial/plugins/whichkey.lua
@@ -0,0 +1,14 @@
+return {
+  'folke/which-key.nvim',
+  event = 'VeryLazy',
+  opts_extend = { 'spec' },
+  opts = {
+    preset = 'helix',
+    spec = {
+      { '<leader>w', group = 'windows'},
+      { '<leader>f', group = 'file/find'},
+      { '<leader>t', group = 'tabs'},
+      { '<leader>b', group = 'buffer'}, 
+    }
+  }
+}
