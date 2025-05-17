891a2330f5f871de053d0afd6bfadbf068ab659c
Alexandre Costa <coaxial@192.168.1.236>
2025-05-17 14:30:03 +0100

    chore(nvim): add lazyvim + custom alias

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -1,4 +1,6 @@
 if status is-interactive
-  # Commands to run in interactive sessions can go here
-  starship init fish | source
+    # Commands to run in interactive sessions can go here
+    starship init fish | source
 end
+
+alias neovim="NVIM_APPNAME=nvim-custom nvim"
